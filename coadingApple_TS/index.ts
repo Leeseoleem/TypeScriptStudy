a3ee3c458dad922871d2ecd13bc13e8e089af83b
Leeseoleem <leeseorim0029@gmail.com>
2024-12-10 02:34:41 +0900

    19per
    들었음

--- coadingApple_TS/index.ts
+++ coadingApple_TS/index.ts
@@ -71,3 +71,57 @@ let 모쏠: boolean = true;
 // 그러나 모두 타입 지정이 필요한 건 아니다
 let 회원명 = "하이요"; // 이렇게 지정 안해도 알아서 지정해준다
 // 회원명 = 123; 이렇게 하면 오류난다
+
+// 3. 타입 지정하기 애매할 때: union, any, unkown
+// 1) union
+let 여러가지: string | number = 1;
+let 어레지: (string | number)[] = ["hi", 1]; // 괄호 안치면 의미 바뀜
+let 오브젝트: { [key: string]: string | number } = {
+  a: 1,
+  b: "2",
+};
+// 2) any(걍 기본 js랑 비슷한듯)
+let all: any;
+all = "사람";
+all = "10";
+// 3) unkown: any와 유사, 모든 자료형 허용 => any보다 안전
+let 몰라: unknown;
+몰라 = "1";
+
+// any | unkown의 차이
+let 암거나: any = "10";
+let 몰루: unknown = "10";
+let 답: number = 10 - 암거나;
+// let 답2: number = 10 - 몰루; => 에러 발생
+
+// 4. 타입스크립트의 단점: 엄격함
+let 내나이: number | boolean;
+// 내나이 + 1; => 에러: boolean 타입일 수 있기 때문에
+let 가: unknown = 1;
+// 가 + 1 => 에러: TS는 엄격하게 숫자 타입만 계산 허용
+
+// 5. 함수에서 타입 지정하기
+function 함수_예시(a: number): number {
+  return a * 2;
+}
+// 함수의 파라미터 : 변수 생성과 동일 => 타입 설정 가능
+// void 타입: 아무것도 return 하지 않는 함수 타입
+function 함수_예시2(a: number): void {
+  // return a * 2; => 에러 발생
+}
+// 함수의 파라미터에 타입을 지정한 경우 => 함수 선언 시 파라미터 작성 안하면 오류
+// 해결방법? 파라미터 옆에 물음표 추가하자: 파라미터변수? : 타입
+// ?는 undefined 타입과 동일
+// 함수_예시2(); => 인수가 없다고 에러 발생
+function 함수_예시3(x?: string): void {}
+함수_예시3();
+
+// 5. 타입 확정하기
+// 1) Narrowing
+function 양다리함수(x: number | string) {
+  if (typeof x === "number") return x + 1;
+}
+// 2) assertion: 타입 덮어쓰기
+function 문어함수(x: number | string) {
+  return (x as number) + 1;
+}
--- coadingApple_TS/part1_q.ts
+++ coadingApple_TS/part1_q.ts
@@ -31,3 +31,57 @@ let project: {
   days: 30,
   started: true,
 };
+
+// (숙제1) 다음 변수 4개에 타입을 지정해봅시다.
+let user: string = "kim";
+let age: undefined | number = undefined;
+let married: boolean = false;
+let 철수: (string | number | undefined | boolean)[] = [user, age, married];
+
+// (숙제2) 학교라는 변수에 타입지정해보십시오.
+let 학교: {
+  score: (number | boolean)[];
+  teacher: string;
+  friend: string | string[];
+} = {
+  score: [100, 97, 84],
+  teacher: "Phil",
+  friend: "John",
+};
+학교.score[4] = false;
+학교.friend = ["Lee", 학교.teacher];
+
+// (숙제1) 이름을 파라미터로 입력하면 콘솔창에 "안녕하세요 홍길동"을 출력해주고
+function 반가워요(name?: string) {
+  if (name) {
+    console.log("안녕하세요");
+  } else {
+    console.log("이름이 없습니다");
+  }
+}
+
+// (숙제2) 함수에 숫자 또는 문자를 집어넣으면 자릿수를 세어 출력해주는 함수를 만들어보십시오.
+function 자릿수세기(X: number | string): number {
+  return X.toString().length;
+}
+
+// (숙제3) 결혼 가능 확률을 알려주는 함수를 만들어봅시다.
+function 결혼가능여부(
+  월소득: number,
+  집보유여부: boolean,
+  매력점수: string
+): string | void {
+  let total = 월소득 + (집보유여부 ? 500 : 0) + (매력점수 === "상" ? 100 : 0);
+  return total > 600 && "결혼가능";
+}
+
+// 숫자와 문자가 섞인 array를 입력하면 [1,2,3] 이렇게 숫자로 깔끔하게 변환되어 나오는 클리닝함수를 만들어오고 타입지정까지 확실히 해보십시오.
+function 클리닝함수(x: (number | string)[]): number[] {
+  return x.map((x) => Number(x));
+}
+
+// (숙제2) 다음과 같은 함수를 만들어보십시오.
+//  object 자료를 파라미터로 집어넣으면 그 선생님이 가르치고 있는 과목중 맨 뒤의 1개를 return 해주는 함수를 만들어봅시다.
+function 과목추출(x: { subject: string | string[] }) {
+  return typeof x.subject === "string" ? x.subject : x.subject[-1];
+}
